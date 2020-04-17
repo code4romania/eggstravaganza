8f0705dff3ccd6a80cd166056eddebfc4f7c9124
Andrei Ioniță <hi@andrei.io>
2020-04-17 16:28:41 +0100

    Config fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,20 @@
 import messages from './i18n';
 export default {
     mode: 'universal',
+    env: {
+        BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
+    },
     /*
      ** Headers of the page
      */
     head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-        ],
+        meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
     /*
      ** Customize the progress-bar color
      */
-    loading: { color: '#fff' },
+    loading: false,
     /*
      ** Global CSS
      */
